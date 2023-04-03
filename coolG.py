e7c90a86357fbaaddaf7c890f4f32fdd0fac7834
Travis Metcalfe <travis@whitedwarf.org>
2023-04-03 14:55:18 -0600

    Added code for G8 dwarfs

---
+++ coolG.py
@@ -0,0 +1,187 @@
+import numpy as np
+
+##########################################################
+# Finley & Matt (2017/2018) Torque Formulations ##########
+# Version 1.0 : 12th April 2018 ##########################
+##########################################################
+# Function:                                              #
+# FM18_surf-Takes polar strengths of the lowest harmonic #
+#	    components and predicts an average Alfv\'en  #
+#	    radius which produces a braking torque on 	 #
+#	    the star.                                    #
+##########################################################
+# Input Parameters:                                      #
+# Bdip- Polar Dipole Strength in Gauss                   #
+# Bquad- Polar Quadrupole Strength in Gauss              #
+# Boct- Polar Octupole Strength in Gauss                 #
+# Mdot- Mass Loss Rate in Solar per Year                 #
+# Period- Rotation Period in Days                        #
+# Mass- Stellar Mass in Solar Units                      #
+# Radius- Stellar Radius in Solar Units                  #
+# Info- Print Topology and Rotation Information          #
+##########################################################
+
+def FM18_surf(Bdip, Bquad, Boct, Mdot, Period, Mass, Radius, Info):
+
+	#########################################################
+	#Stellar Parameters:                                    #
+	#########################################################
+	R_star = 6.96e10*(Radius)
+	M_star = 1.99e33*(Mass)
+	Omega_star = 2*np.pi/Period/24/3600
+	v_esc = np.sqrt( 2*(6.67e-8)*M_star/R_star )
+	f = Omega_star*R_star/v_esc*np.sqrt(2)
+	if (f < 0.1 and Info==True):
+		print('SLOW ROTATOR REGIME, f = '+str(f))
+	elif (Info==True):
+		print('FAST ROTATOR REGIME, f = '+str(f))
+	Btot = abs(Bdip) + abs(Bquad) + abs(Boct)
+	Mdot = Mdot*( (2e-14)*(1.99e33)/(365.25*24*3600) )
+	Upsilon = Btot**2*R_star**2/Mdot/v_esc
+	Rdip = Bdip/Btot
+	Rquad = Bquad/Btot
+	Roct = Boct/Btot
+
+
+	#########################################################
+	# Derived Simulation Parameters:                        #
+	#########################################################
+	K1 = 1.53
+	m1 = 0.229
+	K2 = 1.70
+	m2 = 0.134
+	K3 = 1.80
+	m3 = 0.087
+	Kvr = 0.2
+
+	#########################################################
+	# Calculate Average Alfv\'en Radius using equation (17) #
+	#########################################################
+	RA_dip = K1*(Rdip)**(2*m1)*(Upsilon/np.sqrt(1+f**2/Kvr**2))**(m1)
+	RA_quad = K2*(Rquad+Rdip)**(2*m2)*(Upsilon/np.sqrt(1+f**2/Kvr**2))**(m2)
+	RA_oct = K3*(Roct+Rquad+Rdip)**(2*m3)*(Upsilon/np.sqrt(1+f**2/Kvr**2))**(m3)
+	##################### Max Function ######################
+	if ( RA_dip>RA_quad and RA_dip>RA_oct and Rdip>0. ):
+		RA = RA_dip
+		if (Info==True):
+			print('DIPOLE DOMINATED')
+	elif (RA_quad>RA_dip and RA_quad>RA_oct and Rquad>0.):
+		RA=(RA_quad)
+		if (Info==True):
+			print('MULTIPOLAR DOMINATED')
+	else:
+		RA=(RA_oct)
+		if (Info==True):
+			print('MULTIPOLAR DOMINATED')
+	#########################################################
+
+	#########################################################
+	#Calculate Angular Momentum Loss Rate, equation (14)    #
+	#########################################################
+	Torque = Mdot*Omega_star*R_star**2*np.power(RA,2)
+
+	return Upsilon, RA, Torque
+
+
+##########################################################
+# Metcalfe et al. (2022) Torque Analysis #################
+# Version 1.0 :  8th April 2022 ##########################
+##########################################################
+# Function:                                              #
+# torqueDiagnostic-Calculates the FM18 torque for StarA  #
+#	    and StarB, and evaluates the relative        #
+#	    influence of changing each stellar parameter #
+#	    in the braking law.                          #
+##########################################################
+# Input Parameters:                                      #
+# fromStarA- Dictionary* of StarA Properties             #
+# toStarB- Dictionary* of StarB Properties               #
+# RhkFieldChange- Change in <B> given difference in R'HK #
+# Dictionary*- Name, Bdip, Bquad, Boct, Mdot, Period,    #
+#	    Mass, and Radius.                            #
+##########################################################
+
+
+def torqueDiagnostic(fromStarA,toStarB,FieldChange,ndec=5):
+
+	#########################################################
+	#StarA Wind Torque:                                     #
+	#########################################################
+	print('['+fromStarA["Name"]+', Bd='+str(fromStarA["Bdip"])+', Bq='+str(fromStarA["Bquad"])+', Bo='+str(fromStarA["Boct"])+']')
+	Upsilon, RA, Torque1 = FM18_surf(Bdip=fromStarA["Bdip"], Bquad=fromStarA["Bquad"], Boct=fromStarA["Boct"], Mdot=fromStarA["Mdot"], Period=fromStarA["Period"], Mass=fromStarA["Mass"], Radius=fromStarA["Radius"], Info=False)
+	print('Torque:',round(Torque1/1e30,ndec),'x10^30erg') #fiducial model StarA
+	print(' ')
+	#########################################################
+
+	# COMPARE TO StarA MODEL #
+	print('# COMPARE TO '+fromStarA["Name"]+' MODEL #')
+	print(' ')
+
+	#########################################################
+	#StarB Wind Torque:                                     #
+	#########################################################
+	print('['+toStarB["Name"]+', Bd='+str(toStarB["Bdip"])+', Bq='+str(toStarB["Bquad"])+', Bo='+str(toStarB["Boct"])+']')
+	Upsilon, RA, Torque2 = FM18_surf(Bdip=toStarB["Bdip"], Bquad=toStarB["Bquad"], Boct=toStarB["Boct"], Mdot=toStarB["Mdot"], Period=toStarB["Period"], Mass=toStarB["Mass"], Radius=toStarB["Radius"], Info=False)
+	print('Torque:',round(Torque2/1e30,ndec),'x10^30erg') #fiducial model StarB
+	print('['+fromStarA["Name"]+' -> '+toStarB["Name"]+']:',round(Torque2/Torque1-1.,ndec))
+	#########################################################
+
+	#########################################################
+	#Evaluate the Influence of Changing Each Parameter:     #
+	#########################################################
+	print('#parameter study#')
+	Upsilon, RA, Torque3 = FM18_surf(Bdip=FieldChange*fromStarA["Bdip"], Bquad=FieldChange*fromStarA["Bquad"], Boct=FieldChange*fromStarA["Boct"], Mdot=fromStarA["Mdot"], Period=fromStarA["Period"], Mass=fromStarA["Mass"], Radius=fromStarA["Radius"], Info=False)
+	print('<B>:',round(Torque3/Torque1-1.,ndec))
+	Upsilon, RA, Torque = FM18_surf(Bdip=toStarB["Bdip"], Bquad=toStarB["Bquad"], Boct=toStarB["Boct"], Mdot=fromStarA["Mdot"], Period=fromStarA["Period"], Mass=fromStarA["Mass"], Radius=fromStarA["Radius"], Info=False)
+	print('Morph:',round(Torque/Torque3-1.,ndec)) # StarB morphology at same <B>
+	print('both:',round(Torque/Torque1-1.,ndec))
+	Upsilon, RA, Torque = FM18_surf(Bdip=fromStarA["Bdip"], Bquad=fromStarA["Bquad"], Boct=fromStarA["Boct"], Mdot=toStarB["Mdot"], Period=fromStarA["Period"], Mass=fromStarA["Mass"], Radius=fromStarA["Radius"], Info=False)
+	print('Mdot:',round(Torque/Torque1-1.,ndec))
+	Upsilon, RA, Torque = FM18_surf(Bdip=fromStarA["Bdip"], Bquad=fromStarA["Bquad"], Boct=fromStarA["Boct"], Mdot=fromStarA["Mdot"], Period=toStarB["Period"], Mass=fromStarA["Mass"], Radius=fromStarA["Radius"], Info=False)
+	print('Prot:',round(Torque/Torque1-1.,ndec))
+	Upsilon, RA, Torque = FM18_surf(Bdip=fromStarA["Bdip"], Bquad=fromStarA["Bquad"], Boct=fromStarA["Boct"], Mdot=fromStarA["Mdot"], Period=fromStarA["Period"], Mass=toStarB["Mass"], Radius=fromStarA["Radius"], Info=False)
+	print('M:',round(Torque/Torque1-1.,ndec))
+	Upsilon, RA, Torque = FM18_surf(Bdip=fromStarA["Bdip"], Bquad=fromStarA["Bquad"], Boct=fromStarA["Boct"], Mdot=fromStarA["Mdot"], Period=fromStarA["Period"], Mass=fromStarA["Mass"], Radius=toStarB["Radius"], Info=False)
+	print('R:',round(Torque/Torque1-1.,ndec))
+	print(' ')
+	#########################################################
+
+# FIDUCIAL MODELS #
+_HD101501 = {"Name":'HD101501', "Bdip":11.5, "Bquad":12.0, "Boct":6.12, "Mdot":9.6, "Period":17, "Mass":0.94, "Radius":0.845,
+            "un_Bdip":['N/A','N/A'], "un_Bquad":['N/A','N/A'], "un_Boct":['N/A','N/A'], "un_Mdot":['N/A','N/A'], "un_Period":['N/A','N/A'], "un_Mass":['N/A','N/A'], "un_Radius":['N/A','N/A']}
+
+_HD10700 = {"Name":'HD10700', "Bdip":0.77, "Bquad":0.0, "Boct":0.0, "Mdot":0.1, "Period":34.0, "Mass":0.783, "Radius":0.816,
+            "un_Bdip":['N/A','N/A'], "un_Bquad":['N/A','N/A'], "un_Boct":['N/A','N/A'], "un_Mdot":['N/A','N/A'], "un_Period":['N/A','N/A'], "un_Mass":['N/A','N/A'], "un_Radius":['N/A','N/A']}
+
+# INCLINATION #
+_HD10700_i35 = {"Name":'HD10700_i35', "Bdip":0.89, "Bquad":0.0, "Boct":0.0, "Mdot":0.1, "Period":34.0, "Mass":0.783, "Radius":0.816,
+            "un_Bdip":['N/A','N/A'], "un_Bquad":['N/A','N/A'], "un_Boct":['N/A','N/A'], "un_Mdot":['N/A','N/A'], "un_Period":['N/A','N/A'], "un_Mass":['N/A','N/A'], "un_Radius":['N/A','N/A']}
+
+# MORPHOLOGY #
+_HD10700_Bq = {"Name":'HD10700_Bq', "Bdip":0.0, "Bquad":2.94, "Boct":0.0, "Mdot":0.1, "Period":34.0, "Mass":0.783, "Radius":0.816,
+            "un_Bdip":['N/A','N/A'], "un_Bquad":['N/A','N/A'], "un_Boct":['N/A','N/A'], "un_Mdot":['N/A','N/A'], "un_Period":['N/A','N/A'], "un_Mass":['N/A','N/A'], "un_Radius":['N/A','N/A']}
+_HD10700_Bo = {"Name":'HD10700_Bo', "Bdip":0.0, "Bquad":0.0, "Boct":118.9, "Mdot":0.1, "Period":34.0, "Mass":0.783, "Radius":0.816,
+            "un_Bdip":['N/A','N/A'], "un_Bquad":['N/A','N/A'], "un_Boct":['N/A','N/A'], "un_Mdot":['N/A','N/A'], "un_Period":['N/A','N/A'], "un_Mass":['N/A','N/A'], "un_Radius":['N/A','N/A']}
+
+# MASS-LOSS RATE #
+_HD10700_Mdot = {"Name":'HD10700_Mdot', "Bdip":0.77, "Bquad":0.0, "Boct":0.0, "Mdot":0.46, "Period":34.0, "Mass":0.783, "Radius":0.816,
+            "un_Bdip":['N/A','N/A'], "un_Bquad":['N/A','N/A'], "un_Boct":['N/A','N/A'], "un_Mdot":['N/A','N/A'], "un_Period":['N/A','N/A'], "un_Mass":['N/A','N/A'], "un_Radius":['N/A','N/A']}
+
+
+ndec=5 # Number of decimal places for the outputs
+
+print('# EVOLUTIONARY SEQUENCE #')
+print(' ')
+
+torqueDiagnostic(_HD101501,_HD10700,0.253)   # Delta logR'HK=-0.412
+                                             # Correction (0.5*[M/H])
+                                             # Delta logR'HK=-0.597 => 0.253
+                                             
+torqueDiagnostic(_HD10700,_HD10700_i35,1.0)  # i=35 from debris disk
+
+torqueDiagnostic(_HD10700,_HD10700_Bq,1.0)   # Bq from LBT profile
+torqueDiagnostic(_HD10700,_HD10700_Bo,1.0)   # Bo from LBT profile
+
+torqueDiagnostic(_HD10700,_HD10700_Mdot,1.0) # Mdot from Xray flux
+
+print(' ')
